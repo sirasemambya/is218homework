561b5830ba4f8439a61893e4ddd6bd10531a4788
sirasemambya <sirasemambya@gmail.com>
2016-10-18 12:21:31 -0400

    Arrays and strings

---
+++ 15arrays/search.php
@@ -0,0 +1,6 @@
+<?php
+$array = array(0 => 'blue', 1 => 'red', 2 => 'green', 3 => 'red');
+
+$key = array_search('green', $array); // $key = 2;
+$key = array_search('red', $array);   // $key = 1;
+?>
